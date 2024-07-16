73830bfd0dc8586d822be601992029f4ee2421d5
Pitsi Pitsi <111595711+Cobrizi27@users.noreply.github.com>
2024-07-16 22:21:41 +0200

    Update Importing and Cleaning Data.py

--- Importing and Cleaning Data.py
+++ Importing and Cleaning Data.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-# Import data
 df = pd.read_csv("fcc-forum-pageviews.csv", index_col='date', parse_dates=True)
 
-# Clean data
 df = df[(df['value'] >= df['value'].quantile(0.025)) & (df['value'] <= df['value'].quantile(0.975))]
