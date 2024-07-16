8ebe9ea9034b7d9c1b9a3366ab3fdc2e71ff8087
Pitsi Pitsi <111595711+Cobrizi27@users.noreply.github.com>
2024-07-16 22:25:00 +0200

    Update time_series_visualizer.py

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -2,10 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Import data
+
 df = pd.read_csv("fcc-forum-pageviews.csv", index_col='date', parse_dates=True)
 
-# Clean data
 df = df[(df['value'] >= df['value'].quantile(0.025)) & (df['value'] <= df['value'].quantile(0.975))]
 
 def draw_line_plot():
